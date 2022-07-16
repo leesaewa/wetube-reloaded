16ef92f4ee11ba17aea8c1c1ce23e9f03f865f4b
leesaewa <saewa0424@gmail.com>
2022-07-16 19:09:35 +0900

    220716 TIL: part2_github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,10 @@
 import User from "../models/User";
+import fetch from "cross-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "Create Account" });
 export const postJoin = async (req, res) => {
-  console.log(req.body);
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
@@ -22,7 +22,13 @@ export const postJoin = async (req, res) => {
   }
 
   try {
-    await User.create({ name, username, email, password, password2, location });
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
     return res.redirect("/login");
   } catch (error) {
     return res.status(400).render("join", {
@@ -39,7 +45,7 @@ export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
   //유저가 존재하는지 체크
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -58,8 +64,98 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("edit user");
-export const remove = (req, res) => res.send("delete user");
+//
+// github login
+//
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    let user = await User.findOne({ email: emailObj.email });
+    console.log(user);
+    console.log("check_0");
+    // 찾은 email과 같은 email을 가진 user가 DB에 있다면, 그 user를 login 시킨다
+    // 찾은 email과 같은 email을 가진 user가 DB에 없다면, 그 user의 계정을 생성한다 (join)
+    if (!user) {
+      console.log("check_1");
+      console.log(user);
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      console.log("check_2");
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+};
 
-export const logout = (req, res) => res.send("logout");
+//
+// log out
+
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
+
+//
+//
+//
+export const edit = (req, res) => res.send("edit user");
+
 export const see = (req, res) => res.send("see user");
